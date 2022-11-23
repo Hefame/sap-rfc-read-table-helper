276c4ac402ef695e215e2c33ee2ef386297ef483
Alejandro Abad <alejandro.abad@hefame.es>
2022-11-23 17:56:19 +0100

    Mejora en la partición de la clausula WHERE
    Ahora la función intenta no partir palabras a la mitad, de modo que se provoca que falle la consulta a SAP

--- index.js
+++ index.js
@@ -1,74 +1,93 @@
+const splitWhereClause = (where) => {
 
+	let chunks = [];
+	let lastIndexOfSpace = 0;
+	let lastCutIndex = 0;
+
+	for (let i = 0; i < where.length; i++) {
+		if (where.charAt(i) === ' ') lastIndexOfSpace = i
+
+		if (i - lastCutIndex >= 72) {
+			if (!lastIndexOfSpace) lastIndexOfSpace = i
+			chunks.push(where.substring(lastCutIndex, lastIndexOfSpace));
+			lastCutIndex = lastIndexOfSpace + 1;
+		}
+	}
+
+	chunks.push(where.substring(lastCutIndex, where.length));
+	return chunks;
+
+}
 
 const rfcReadTable = async (sapClient, { table, fields, where, skip = 0, limit = 0, fieldNameMap = null, rowMapFn = null }) => {
 
-    const query = {
-        QUERY_TABLE: table,
-        ROWSKIPS: skip,
-        ROWCOUNT: limit,
-		OPTIONS: where?.length ? where.split(/(.{72})/).filter(o => o) : [],
-        FIELDS: Array.isArray(fields) ? fields.map(fieldName => {
-            return { FIELDNAME: fieldName }
-        }) : []
-    }
+	const query = {
+		QUERY_TABLE: table,
+		ROWSKIPS: skip,
+		ROWCOUNT: limit,
+		OPTIONS: where?.length ? splitWhereClause(where) : [],
+		FIELDS: Array.isArray(fields) ? fields.map(fieldName => {
+			return { FIELDNAME: fieldName }
+		}) : []
+	}
 
 	if (!sapClient) return query;
 
-    const rfcResult = await sapClient.call("RFC_READ_TABLE", query)
+	const rfcResult = await sapClient.call("RFC_READ_TABLE", query)
 
-    const fieldFnList = rfcResult.FIELDS.map((fieldDescription, idx) => {
+	const fieldFnList = rfcResult.FIELDS.map((fieldDescription, idx) => {
 
-        let startIdx = parseInt(fieldDescription.OFFSET, 10);
-        let endIdx = startIdx + parseInt(fieldDescription.LENGTH, 10);
-        let fieldName = fieldNameMap?.[idx] ? fieldNameMap[idx] : fieldDescription.FIELDNAME;
+		let startIdx = parseInt(fieldDescription.OFFSET, 10);
+		let endIdx = startIdx + parseInt(fieldDescription.LENGTH, 10);
+		let fieldName = fieldNameMap?.[idx] ? fieldNameMap[idx] : fieldDescription.FIELDNAME;
 
-        switch (fieldDescription.TYPE) {
-            case 'N': return (tableLine, resultObj) => {
-                resultObj[fieldName] = parseInt(tableLine.substring(startIdx, endIdx), 10);
-            }
+		switch (fieldDescription.TYPE) {
+			case 'N': return (tableLine, resultObj) => {
+				resultObj[fieldName] = parseInt(tableLine.substring(startIdx, endIdx), 10);
+			}
 
-            case 'P': return (tableLine, resultObj) => {
-                resultObj[fieldName] = parseFloat(tableLine.substring(startIdx, endIdx));
-            }
+			case 'P': return (tableLine, resultObj) => {
+				resultObj[fieldName] = parseFloat(tableLine.substring(startIdx, endIdx));
+			}
 
-            default: return (tableLine, resultObj) => {
-                resultObj[fieldName] = tableLine.substring(startIdx, endIdx);
-            }
-        }
+			default: return (tableLine, resultObj) => {
+				resultObj[fieldName] = tableLine.substring(startIdx, endIdx);
+			}
+		}
 
-    });
+	});
 
-    if (rowMapFn) {
-        return rfcResult.DATA.map(line => {
-            const r = {};
-            fieldFnList.forEach(fieldFn => {
-                fieldFn(line.WA, r)
-                
-            })
-            return rowMapFn(r);
-        })
-    }
+	if (rowMapFn) {
+		return rfcResult.DATA.map(line => {
+			const r = {};
+			fieldFnList.forEach(fieldFn => {
+				fieldFn(line.WA, r)
 
-    return rfcResult.DATA.map(line => {
-        const r = {};
-        fieldFnList.forEach(fieldFn => {
-            fieldFn(line.WA, r)
-        })
-        return r;
-    })
+			})
+			return rowMapFn(r);
+		})
+	}
+
+	return rfcResult.DATA.map(line => {
+		const r = {};
+		fieldFnList.forEach(fieldFn => {
+			fieldFn(line.WA, r)
+		})
+		return r;
+	})
 
 }
 
 rfcReadTable.dateTimeUnion = (dats, tims) => {
 
-    return new Date(
-        parseInt(dats.substring(0,4)),
-        parseInt(dats.substring(4,6)),
-        parseInt(dats.substring(6,8)),
-        parseInt(tims.substring(0,2)),
-        parseInt(tims.substring(2,4)),
-        parseInt(tims.substring(4,6)),
-    )
+	return new Date(
+		parseInt(dats.substring(0, 4)),
+		parseInt(dats.substring(4, 6)),
+		parseInt(dats.substring(6, 8)),
+		parseInt(tims.substring(0, 2)),
+		parseInt(tims.substring(2, 4)),
+		parseInt(tims.substring(4, 6)),
+	)
 
 }
 
